65681f26531e66ae07528f9151ea5c5077332aad
Yuri Mello <yuridemeello@gmail.com>
2020-07-30 19:05:47 -0300

    Rota para criação de appointments e estruturas de pastas para rotas

---
+++ src/routes/appointments.routes.ts
@@ -0,0 +1,22 @@
+import { Router } from 'express';
+import { uuid } from 'uuidv4';
+
+const routes = Router();
+
+const appointments = [];
+
+routes.post('/', (request, response) => {
+  const { provider, date } = request.body;
+
+  const appointment = {
+    id: uuid(),
+    provider,
+    date,
+  };
+
+  appointments.push(appointment);
+
+  return response.json(appointment);
+});
+
+export default routes;
