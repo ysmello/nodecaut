17aba9cb8ae9f3d0e2abc698eaad7c0e00f2df95
Yuri Mello <yuridemeello@gmail.com>
2020-07-30 18:39:54 -0300

    Configurações de debug

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
 
-app.get('/', (request, response) => {
-  response.json({ message: 'Hello World!!!' });
-});
+app.use(express.json());
+app.use(routes);
 
 app.listen(8081);
