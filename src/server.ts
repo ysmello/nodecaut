0aaf6122b5f14924b6df55deda7051587065d7a3
Yuri Mello <yuridemeello@gmail.com>
2020-07-30 18:21:56 -0300

    Padrões do projeto

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,9 @@
-import express from "express";
+import express from 'express';
 
 const app = express();
 
-app.get("/", (request, response) => {
-  response.json({ message: "Hello World!!!" });
+app.get('/', (request, response) => {
+  response.json({ message: 'Hello World!!!' });
 });
 
 app.listen(8081);
