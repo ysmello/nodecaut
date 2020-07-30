136f37b755fd2a36d6ebcfb176857284f4cbf8e2
Yuri Mello <yuridemeello@gmail.com>
2020-07-30 19:21:59 -0300

    Validação em criação de appointments para datas iguais

--- src/routes/appointments.routes.ts
+++ src/routes/appointments.routes.ts
@@ -1,17 +1,35 @@
 import { Router } from 'express';
 import { uuid } from 'uuidv4';
+import { startOfHour, parseISO, isEqual } from 'date-fns';
 
 const routes = Router();
 
-const appointments = [];
+interface Appointment {
+  provider: string;
+  date: Date;
+}
+
+const appointments: Appointment[] = [];
 
 routes.post('/', (request, response) => {
   const { provider, date } = request.body;
 
+  const parsedDate = startOfHour(parseISO(date));
+
+  const findAppointmentInSameDate = appointments.find(appointment =>
+    isEqual(parsedDate, appointment.date),
+  );
+
+  if (findAppointmentInSameDate) {
+    return response
+      .status(400)
+      .json({ message: 'This appointment is already booked' });
+  }
+
   const appointment = {
     id: uuid(),
     provider,
-    date,
+    date: parsedDate,
   };
 
   appointments.push(appointment);
