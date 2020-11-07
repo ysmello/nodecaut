128b986cf4b08d048193b885c9da6a00523702e1
Yuri Mello <yuridemeello@gmail.com>
2020-11-06 22:06:23 -0300

    teste unitario para criar um novo appointment

--- src/modules/appointments/services/CreateAppointementService.spec.ts
+++ src/modules/appointments/services/CreateAppointementService.spec.ts
@@ -1,3 +1,19 @@
-test('sum', () => {
-  expect(1 + 2).toBe(3);
+import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
+import CreateAppointmentService from './CreateAppointementService';
+
+describe('CreateAppointement', () => {
+  it('should be able to create a new appointment', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+
+    const appointment = await createAppointment.execute(
+      { 
+        date: new Date,
+        provider_id: '123123'
+      }
+    )
+
+    expect(appointment).toHaveProperty('id');
+    expect(appointment.provider_id).toBe('123123');
+  })
 })
