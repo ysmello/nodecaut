ffdf3c7ff396277d03773b323d1bcf90f990a936
Yuri Mello <yuridemeello@gmail.com>
2020-11-06 22:34:52 -0300

    testes de agendamento

--- src/modules/appointments/services/CreateAppointementService.spec.ts
+++ src/modules/appointments/services/CreateAppointementService.spec.ts
@@ -1,3 +1,4 @@
+import AppError from '@shared/errors/AppError';
 import FakeAppointmentsRepository from '../repositories/fakes/FakeAppointmentsRepository'
 import CreateAppointmentService from './CreateAppointementService';
 
@@ -15,5 +16,26 @@ describe('CreateAppointement', () => {
 
     expect(appointment).toHaveProperty('id');
     expect(appointment.provider_id).toBe('123123');
+  });
+
+  it('should be able to create two appointments on the same time', async () => {
+    const fakeAppointmentRepository = new FakeAppointmentsRepository();
+    const createAppointment = new CreateAppointmentService(fakeAppointmentRepository);
+
+    const appointmentDate = new Date(2020, 4, 1)
+
+    await createAppointment.execute(
+      { 
+        date: appointmentDate,
+        provider_id: '123123'
+      }
+    )
+
+    expect(createAppointment.execute(
+      { 
+        date: appointmentDate,
+        provider_id: '123123'
+      }
+    )).rejects.toBeInstanceOf(AppError);
   })
 })
