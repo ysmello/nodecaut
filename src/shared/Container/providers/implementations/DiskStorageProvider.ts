a6d20e8fd94e2db5139fabba8ad011e7deaf6771
Yuri Mello <yuridemeello@gmail.com>
2020-11-08 17:56:17 -0300

    Provider de storage

---
+++ src/shared/Container/providers/implementations/DiskStorageProvider.ts
@@ -0,0 +1,30 @@
+import fs from 'fs';
+import path from 'path';
+import uploadConfigs from '@config/upload';
+
+import IStorageProvider from '../models/IStorageProvider';
+
+class DiskStorageProvider implements IStorageProvider {
+  public async saveFile(file: string): Promise<string> {
+    await fs.promises.rename(
+      path.resolve(uploadConfigs.tmpFolder, file),
+      path.resolve(uploadConfigs.uploadsFolder, file)
+    );
+
+    return file
+  }
+
+  public async deleteFile(file: string): Promise<void> {
+    const filePath = path.resolve(uploadConfigs.uploadsFolder, file);
+
+    try {
+      await fs.promises.stat(filePath)
+    } catch {
+      return;
+    }
+
+    await fs.promises.unlink(filePath);
+  }
+}
+
+export default DiskStorageProvider;
