4c28e6798e589a73d343e59fe1dea2ad9b12c745
Yuri Mello <yuridemeello@gmail.com>
2020-11-16 23:55:47 -0300

    filtrando agendamento por mês

--- src/modules/appointments/repositories/IAppointmentsRepository.ts
+++ src/modules/appointments/repositories/IAppointmentsRepository.ts
@@ -1,7 +1,11 @@
 import Appointment from '../infra/typeorm/entities/Appointment';
-import ICreateAppointment from '../dtos/ICreateAppointmentDTO'
+import ICreateAppointment from '../dtos/ICreateAppointmentDTO';
+import IFindAllInMonthFromProviderDTO from '../dtos/IFindAllInMonthFromProviderDTO';
 
 export default interface IAppointmentsRepository {
-    create(data: ICreateAppointment): Promise<Appointment>;
-    findByDate(date: Date): Promise<Appointment | undefined>;
+  create(data: ICreateAppointment): Promise<Appointment>;
+  findByDate(date: Date): Promise<Appointment | undefined>;
+  findAllInMonthFromProvider(
+    data: IFindAllInMonthFromProviderDTO,
+  ): Promise<Appointment[]>;
 }
---
+++ src/modules/appointments/services/ListProviderMonthAvailabilityService.spec.ts
@@ -0,0 +1,52 @@
+import FakeAppoitmentsRepository from '@modules/appointments/repositories/fakes/FakeAppointmentsRepository';
+import ListProviderMonthAvailabilityService from './ListProviderMonthAvailabilityService';
+
+let fakeAppoitmentsRepository: FakeAppoitmentsRepository;
+let listProviderMonthAvailability: ListProviderMonthAvailabilityService;
+
+describe('ListProviders', () => {
+  beforeEach(() => {
+    fakeAppoitmentsRepository = new FakeAppoitmentsRepository();
+
+    listProviderMonthAvailability = new ListProviderMonthAvailabilityService(
+      fakeAppoitmentsRepository,
+    );
+  });
+
+  it('should be able to list the month availability from provider', async () => {
+    await fakeAppoitmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 3, 1, 8, 0, 0),
+    });
+
+    await fakeAppoitmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 1, 8, 0, 0),
+    });
+
+    await fakeAppoitmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 1, 10, 0, 0),
+    });
+
+    await fakeAppoitmentsRepository.create({
+      provider_id: 'user',
+      date: new Date(2020, 4, 1, 21, 0, 0),
+    });
+
+    const availability = await listProviderMonthAvailability.execute({
+      provider_id: 'user',
+      year: 2020,
+      month: 5,
+    });
+
+    expect(availability).toEqual(
+      expect.arrayContaining([
+        { day: 19, available: true },
+        { day: 20, available: false },
+        { day: 21, available: false },
+        { day: 22, available: true },
+      ]),
+    );
+  });
+});
