407f4d1c9eb331a97cd428f6da33a87aefd8b93e
Yuri Mello <yuridemeello@gmail.com>
2020-11-19 08:59:33 -0300

    Email pelo Amazon SES

---
+++ src/config/mail.ts
@@ -0,0 +1,21 @@
+interface IMailConfig {
+  driver: 'ethereal' | 'ses';
+
+  defaults: {
+    from: {
+      email: string;
+      name: string;
+    };
+  };
+}
+
+export default {
+  driver: process.env.MAIL_DRIVER || 'ethereal',
+
+  defaults: {
+    from: {
+      email: 'yuridemeelo@gmail.com',
+      name: 'Yuri de Mello',
+    },
+  },
+} as IMailConfig;
