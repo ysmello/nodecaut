be56e7c4502ac1acd89822d32beaae50bc965722
Yuri Mello <yuridemeello@gmail.com>
2020-11-20 02:21:24 -0300

    Removendo cache

--- src/modules/users/services/CreateUserService.ts
+++ src/modules/users/services/CreateUserService.ts
@@ -1,8 +1,9 @@
 import { injectable, inject } from 'tsyringe';
 
 import AppError from '@shared/errors/AppError';
-import IUsersRepository from '../repositories/IUsersRepository'
-import IHashProvider from '../providers/HashProvider/models/IHashProvider'
+import IUsersRepository from '../repositories/IUsersRepository';
+import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import ICacheProvider from '@shared/Container/providers/CacheProvider/models/ICacheProvider';
 
 import User from '@modules/users/infra/typeorm/entities/User';
 
@@ -19,23 +20,36 @@ class CreateUserService {
     private usersRepository: IUsersRepository,
 
     @inject('HashProvider')
-    private hashProvider: IHashProvider
-    ) {}
+    private hashProvider: IHashProvider,
 
-  public async execute({ name, email, password }: IRequest): Promise<User> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+    @inject('RedisCacheProvider')
+    private cacheProvider: ICacheProvider,
+  ) {}
+
+  public async execute({
+    name,
+    email,
+    password,
+  }: IRequest): Promise<User> {
+    const checkUserExists = await this.usersRepository.findByEmail(
+      email,
+    );
 
     if (checkUserExists) {
       throw new AppError('Email adress already used.', 401);
     }
 
-    const hashedPassword = await this.hashProvider.generateHash(password);
+    const hashedPassword = await this.hashProvider.generateHash(
+      password,
+    );
 
     const user = this.usersRepository.create({
       name,
       email,
       password: hashedPassword,
     });
+
+    await this.cacheProvider.invalidatePrefix('providers-list');
 
     return user;
   }
